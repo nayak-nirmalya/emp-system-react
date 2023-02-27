7c1a4d9cb4d54bdd5866b483ce9c550744130089
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-27 23:20:42 +0530

    added header navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className="bg-gray-800">
       <div className="container flex items-center h-16 px-8">
-        <p className="text-white font-bold">Employee Management System!</p>
+        <p
+          onClick={() => navigate("/")}
+          className="text-white font-bold hover:cursor-pointer"
+        >
+          Employee Management System!
+        </p>
       </div>
     </nav>
   );
