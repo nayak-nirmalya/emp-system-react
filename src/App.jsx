05fbdb34cc7a1d589ea85de75181b577c666809c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-27 14:00:00 +0530

    created form for adding employee details

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import "./App.css";
+import AddEmployee from "./components/AddEmployee";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <AddEmployee />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/AddEmployee.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const AddEmployee = () => {
+  return (
+    <div className="flex max-w-2xl h-96 mx-auto shadow border-b">
+      <div className="px-8 py-8 mx-auto">
+        <div className="font-thin text-2xl tracking-wider">
+          <h1>Add New Employee:</h1>
+        </div>
+        <div className="items-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            First Name
+          </label>
+          <input
+            type="text"
+            placeholder="Enter First Name Here"
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+          <label className="block text-gray-600 text-sm font-normal">
+            Last Name
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Last Name Here"
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+          <label className="block text-gray-600 text-sm font-normal">
+            E-Mail
+          </label>
+          <input
+            type="email"
+            placeholder="Enter E-Mail Here"
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
+        <div className="items-center justify-center h-14 w-full my-4 pt-36 space-x-2">
+          <button className="rounded text-white font-semibold bg-green-600 p-1 hover:bg-green-800">
+            Save
+          </button>
+          <button className="rounded text-white font-semibold bg-red-600 p-1 hover:bg-red-800">
+            Clear
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddEmployee;
