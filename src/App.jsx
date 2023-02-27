147d7523619c9567553d364c7df0646cf562a72d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-27 12:16:38 +0530

    made navbar separaet component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800">
+      <div className="container flex items-center h-16 px-8">
+        <p className="text-white font-bold">Employee Management System!</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
