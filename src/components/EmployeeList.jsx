9ae79002b2d7eb53e65630c0a63b79f973028299
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-27 23:12:04 +0530

    added employee component

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import EmployeeService from "../services/EmployeeService";
+import Employee from "./Employee";
 
 const EmployeeList = () => {
   const navigate = useNavigate();
@@ -53,37 +54,7 @@ const EmployeeList = () => {
           {!loading && (
             <tbody className="bg-white">
               {employees.map((employee) => (
-                <tr key={employee.id}>
-                  <td className="text-center px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-500">
-                      {employee.firstName}
-                    </div>
-                  </td>
-                  <td className="text-center px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-500">
-                      {employee.lastName}
-                    </div>
-                  </td>
-                  <td className="text-center px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-500">
-                      {employee.emailId}
-                    </div>
-                  </td>
-                  <td className="text-center px-6 py-4 whitespace-nowrap font-medium text-sm">
-                    <a
-                      href="#"
-                      className="text-indigo-600 hover:text-indigo-800 px-4"
-                    >
-                      Edit
-                    </a>
-                    <a
-                      href="#"
-                      className="text-red-600 hover:text-red-800 px-2"
-                    >
-                      Delete
-                    </a>
-                  </td>
-                </tr>
+                <Employee employee={employee} key={employee.id} />
               ))}
             </tbody>
           )}
